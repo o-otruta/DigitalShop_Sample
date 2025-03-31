2b395c18dba90679fa2c991357b7a0c03d0c4fc7
o-otruta <36052353+o-otruta@users.noreply.github.com>
2025-03-31 05:16:36 +0300

    Add files via upload

---
+++ docs/zudoku/assets/about-CdKhn16O.js
@@ -0,0 +1,7 @@
+import{Z as r,j as t}from"./entry.client-DWncdgUP.js";const i=`This API documentation was created for the DigitalShop sample project.
+Author: Danilo Reztsia
+It provides endpoints for user authentication, product listings, and order processing.`,a=[{depth:1,value:"About",id:"about"}],c=void 0;function o(e){const n={br:"br",h1:"h1",p:"p",...r(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(n.h1,{id:"about",children:"About"}),`
+`,t.jsxs(n.p,{children:["This API documentation was created for the DigitalShop sample project.",t.jsx(n.br,{}),`
+`,"Author: Danilo Reztsia",t.jsx(n.br,{}),`
+`,"It provides endpoints for user authentication, product listings, and order processing."]})]})}function d(e={}){const{wrapper:n}={...r(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o(e)}export{d as default,i as excerpt,c as frontmatter,a as tableOfContents};
+//# sourceMappingURL=about-CdKhn16O.js.map
---
+++ docs/zudoku/assets/installation-p08SuBKh.js
@@ -0,0 +1,6 @@
+import{Z as r,j as t}from"./entry.client-DWncdgUP.js";const l="To install the SDK, clone the repository:",c=[{depth:1,value:"Installation",id:"installation"}],i=void 0;function o(n){const e={code:"code",h1:"h1",p:"p",pre:"pre",...r(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(e.h1,{id:"installation",children:"Installation"}),`
+`,t.jsx(e.p,{children:"To install the SDK, clone the repository:"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{className:"language-bash",children:`git clone https://github.com/o-otruta/DigitalShop_Sample
+`})}),`
+`,t.jsx(e.p,{children:"Include the SDK project in your solution or reference the compiled DLL."})]})}function a(n={}){const{wrapper:e}={...r(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o(n)}export{a as default,l as excerpt,i as frontmatter,c as tableOfContents};
+//# sourceMappingURL=installation-p08SuBKh.js.map
