1995c636a854828b5c46b24f7f660c5078202bf6
o-otruta <36052353+o-otruta@users.noreply.github.com>
2025-03-31 04:35:57 +0300

    ADD zudoku

---
+++ docs/zudoku/assets/installation-D6pv6PiM.js
@@ -0,0 +1,6 @@
+import{Z as r,j as t}from"./entry.client-CKaedWGi.js";const c="To install the SDK, clone the repository:",i=[{depth:1,value:"Installation",id:"installation"}],l=void 0;function o(e){const n={code:"code",h1:"h1",p:"p",pre:"pre",...r(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(n.h1,{id:"installation",children:"Installation"}),`
+`,t.jsx(n.p,{children:"To install the SDK, clone the repository:"}),`
+`,t.jsx(n.pre,{children:t.jsx(n.code,{className:"language-bash",children:`git clone https://github.com/o-otruta/DigitalShop
+`})}),`
+`,t.jsx(n.p,{children:"Include the SDK project in your solution or reference the compiled DLL."})]})}function a(e={}){const{wrapper:n}={...r(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o(e)}export{a as default,c as excerpt,l as frontmatter,i as tableOfContents};
+//# sourceMappingURL=installation-D6pv6PiM.js.map
