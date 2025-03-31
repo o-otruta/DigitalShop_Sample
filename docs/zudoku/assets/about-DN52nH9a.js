814de8e634d2711166a0a46604e4a0968bfc6939
o-otruta <36052353+o-otruta@users.noreply.github.com>
2025-03-31 05:15:46 +0300

    Delete docs/zudoku directory

--- docs/zudoku/assets/about-DN52nH9a.js
+++
@@ -1,7 +0,0 @@
-import{Z as r,j as t}from"./entry.client-CKaedWGi.js";const i=`This API documentation was created for the DigitalShop sample project.
-Author: Oleh Otruta
-It provides endpoints for user authentication, product listings, and order processing.`,a=[{depth:1,value:"About",id:"about"}],c=void 0;function o(e){const n={br:"br",h1:"h1",p:"p",...r(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(n.h1,{id:"about",children:"About"}),`
-`,t.jsxs(n.p,{children:["This API documentation was created for the DigitalShop sample project.",t.jsx(n.br,{}),`
-`,"Author: Oleh Otruta",t.jsx(n.br,{}),`
-`,"It provides endpoints for user authentication, product listings, and order processing."]})]})}function d(e={}){const{wrapper:n}={...r(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o(e)}export{d as default,i as excerpt,c as frontmatter,a as tableOfContents};
-//# sourceMappingURL=about-DN52nH9a.js.map
